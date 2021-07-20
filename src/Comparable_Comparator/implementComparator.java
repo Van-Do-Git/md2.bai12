dd550db974a298e9fa9454ebdf218dd8e1899c97
unknown <mittervan@gmail.com>
2021-07-20 10:39:27 +0700

    third commit

---
+++ src/Comparable_Comparator/implementComparator.java
@@ -0,0 +1,15 @@
+package Comparable_Comparator;
+
+import java.util.Comparator;
+
+public class implementComparator implements Comparator<Student> {
+    @Override
+    public int compare(Student student1, Student student2) {
+        if(student1.getAge()>student2.getAge())
+            return 1;
+        else if (student1.getAge()<student2.getAge())
+            return -1;
+        else
+            return 0;
+    }
+}
